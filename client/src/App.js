0ca86748184b659bfd9c8d366f7fb51953816a6e
Daniel <carreraprogrammer@gmail.com>
2023-12-14 18:44:42 -0500

    Delete unnecessary files and update dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { useNavigate } from 'react-router';
+import { Route, Routes } from 'react-router-dom';
+import { getUser } from './lib/auth';
+import NavBar from './components/NavBar';
+import CompanyPage from './pages/CompanyPage';
+import CreateJobPage from './pages/CreateJobPage';
+import HomePage from './pages/HomePage';
+import JobPage from './pages/JobPage';
+import LoginPage from './pages/LoginPage';
 
 function App() {
+  const navigate = useNavigate();
+  const [user, setUser] = useState(getUser);
+
+  const handleLogin = (user) => {
+    setUser(user);
+    navigate('/');
+  };
+
+  const handleLogout = () => {
+    setUser(null);
+    navigate('/');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar user={user} onLogout={handleLogout} />
+      <main className="section">
+        <Routes>
+          <Route index path="/"
+            element={<HomePage />}
+          />
+          <Route path="/companies/:companyId"
+            element={<CompanyPage />}
+          />
+          <Route path="/jobs/new"
+            element={<CreateJobPage />}
+          />
+          <Route path="/jobs/:jobId"
+            element={<JobPage />}
+          />
+          <Route path="/login"
+            element={<LoginPage onLogin={handleLogin} />}
+          />
+        </Routes>
+      </main>
+    </>
   );
 }
 
