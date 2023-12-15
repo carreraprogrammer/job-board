2546a919d3a0ebe915177293bfc111c6bd7ce72c
Daniel <carreraprogrammer@gmail.com>
2023-12-15 08:50:12 -0500

    Add new GraphQL queries for retrieving company and job information

--- client/src/lib/graphql/queries.js
+++ client/src/lib/graphql/queries.js
@@ -4,10 +4,45 @@ import { GraphQLClient, gql } from "graphql-request";
 
 const client = new GraphQLClient("http://localhost:9000/graphql");
 
+export async function getCompany(id) {
+  const query = gql`
+    query CompanyQuery($id: ID!) {
+      company(id: $id) {
+        id
+        name
+        description
+      }
+    }
+  `;
+  const variables = { id };
+  const { company } = await client.request(query, variables);
+  return company;
+}
+
+export async function getJob(id) {
+  const query = gql`
+    query JobQuery($id: ID!) {
+      job(id: $id) {
+        id
+        title
+        date
+        description
+        company {
+          id
+          name
+        }
+      }
+    }
+  `;
+  const variables = { id };
+  const { job } = await client.request(query, variables);
+  return job;
+}
+
 export async function getJobs() {
   const query = gql`
     {
-      job {
+      jobs {
         id
         title
         date
@@ -20,6 +55,6 @@ export async function getJobs() {
     }
   `;
   
-  const { job } = await client.request(query);
-  return job;
+  const { jobs } = await client.request(query);
+  return jobs;
 }
