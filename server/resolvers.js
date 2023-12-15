63aa91fe26025821d2b5dbb6d93e04baececdbca
Daniel <carreraprogrammer@gmail.com>
2023-12-15 09:35:05 -0500

    Add error handling for not found cases in resolvers

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,10 +1,23 @@
+import { GraphQLError } from "graphql"
 import { getJobs, getJob, getJobsByCompany } from "./db/jobs.js"
 import { getCompany } from "./db/companies.js"
 
 export const resolvers = {
   Query: {
-    company: (_root, { id }) => getCompany(id),
-    job: (_root, { id }) => getJob(id),
+    company: async (_root, { id }) => {
+      const company = await getCompany(id);
+      if(!company) {
+        throw notFoundError(`Company not found: ${id}`);
+      }
+      return company;
+    },
+    job: async (_root, { id }) => {
+      const job = await getJob(id);
+      if(!job) {
+        throw notFoundError(`Job not found: ${id}`);
+      }
+      return job;
+    },
     jobs: () => getJobs(),
   },
 
@@ -24,4 +37,12 @@ export const resolvers = {
 
 function toIsoDate (date) {
   return date.slice(0, 'yyyy-mm-dd'.length);
+}
+
+function notFoundError(message) {
+  return new GraphQLError(message, {
+    extensions: {
+      code: 'NOT_FOUND'
+    }
+  });
 }
