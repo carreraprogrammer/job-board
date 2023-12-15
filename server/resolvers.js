d8a56d77692b67b974c4db89f923c28481c5fe0c
Daniel <carreraprogrammer@gmail.com>
2023-12-15 08:50:04 -0500

    Add company and job resolvers

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,15 +1,25 @@
-import { getJobs } from "./db/jobs.js"
+import { getJobs, getJob } from "./db/jobs.js"
 import { getCompany } from "./db/companies.js"
 
 export const resolvers = {
   Query: {
-    job: () => getJobs(),
+    company: (_root, { id }) => getCompany(id),
+    job: (_root, { id }) => getJob(id),
+    jobs: () => getJobs(),
   },
 
   Job: {
     date: (job) => toIsoDate(job.createdAt),
     company: (job) => {
       return getCompany(job.companyId);
+    }
+  },
+
+  Company: {
+    jobs: async (company) => {
+      const jobs = await getJobs();
+      const companyJobs = jobs.filter((job) => job.companyId === company.id);
+      return companyJobs;
     }
   }
 };
