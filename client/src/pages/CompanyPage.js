ae82154249d85bfccf9f40d1a5abb710e3ddac89
Daniel <carreraprogrammer@gmail.com>
2023-12-15 08:50:16 -0500

    Add useEffect hook to fetch company data

--- client/src/pages/CompanyPage.js
+++ client/src/pages/CompanyPage.js
@@ -1,10 +1,21 @@
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
-import { companies } from '../lib/fake-data';
+import { getCompany } from '../lib/graphql/queries';
 
 function CompanyPage() {
+  const [company, setCompany] = useState([]);
   const { companyId } = useParams();
 
-  const company = companies.find((company) => company.id === companyId);
+  useEffect(() => {
+    getCompany(companyId).then((data) => setCompany(data));
+  }, [companyId]);
+
+  if (!company) return (
+    <div className="notification is-primary">
+      Loading...
+    </div>
+  );
+
   return (
     <div>
       <h1 className="title">
