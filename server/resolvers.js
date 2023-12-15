00f8f97ef3e92df467110e12225838d4ad3ca809
Daniel <carreraprogrammer@gmail.com>
2023-12-14 20:21:48 -0500

    Refactor job resolver and add date field to Job type

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,11 +1,16 @@
+import { get } from "mongoose";
+import { getJobs } from "./db/jobs.js"
+
 export const resolvers = {
   Query: {
-    job: () => {
-      return {
-        id: "test-id",
-        title: "The Title",
-        description: "The Description"
-      };
-    }
+    job: () => getJobs(),
+  },
+
+  Job: {
+    date: (job) => toIsoDate(job.date),
   }
 };
+
+function toIsoDate (date) {
+  return date.toISOString().slice(0, 'yyyy-mm-dd'.length);
+}
